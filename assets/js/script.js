3f7a7a676174e47fe51b30ea00932604e070be2c
Ben <ben.atkinson.official@gmail.com>
2021-09-10 00:18:55 +1000

    Implement get password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 function getPasswordLength() {
+  // set up some variables to hold my constants so they can be altered easily later
+  var minLength = 8;
+  var maxLength = 128;
+  var hintMessage = "Length must be between " + minLength + " and " + maxLength + ".  Please try again.";
+  var lengthMessage = "Choose a password length between " + minLength + " and " + maxLength + " characters.";
+  // get the initial password length
+  var passwordLength = prompt(lengthMessage);
+  // if the password length input was invalid, repeat the process until the user enters a valid input
+  while (passwordLength < minLength || passwordLength > maxLength || !passwordLength) {
+    // since the user has already entered bad data, prepend the hint message as well
+    passwordLength = prompt(hintMessage + "\n\n" + lengthMessage);
+  }
+  // return the password length
+  console.log(passwordLength);
+  return passwordLength;
 
 }
 
